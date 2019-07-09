6979454de7c8642051844f0a77dae00fc9a568dd
Tilak <ayushsatra@gmail.com>
2019-07-10 00:12:36 +0530

    Final Commit

---
+++ FragmentExample/app/src/main/java/com/example/dellayush/fragmentexample/FragmentClass.java
@@ -0,0 +1,21 @@
+package com.example.dellayush.fragmentexample;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentClass extends  Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.top_file_fragment,container,false);
+        return view;
+    }
+}
